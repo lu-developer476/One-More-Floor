f1abea6371b3a0a886f0fff2bdccd1bf696efcb2
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-07-26 03:34:49 -0300

    Remove unused Hazard scene parameter

--- src/objects/Hazard.ts
+++ src/objects/Hazard.ts
@@ -1,2 +1,12 @@
 import Phaser from 'phaser';
-export const addSpikes = (scene: Phaser.Scene, group: Phaser.Physics.Arcade.StaticGroup, x:number,y:number,width:number): void => { for(let px=x;px<x+width;px+=32) group.create(px,y,'spike').setOrigin(0.5,1).refreshBody(); };
+
+export const addSpikes = (
+  group: Phaser.Physics.Arcade.StaticGroup,
+  x: number,
+  y: number,
+  width: number,
+): void => {
+  for (let positionX = x; positionX < x + width; positionX += 32) {
+    group.create(positionX, y, 'spike').setOrigin(0.5, 1).refreshBody();
+  }
+};
